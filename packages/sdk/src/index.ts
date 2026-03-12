096eedde9fb549c8d669cb93cc5850753de538cb
Ladislav Martincik <ladislav.martincik@gmail.com>
2026-03-12 12:09:50 +0100

    feat(sdk): wire barrel exports and exclude tests from build output

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,1 +1,19 @@
-export {};
+export { SandcasterClient } from "./client.js";
+export { parseSSEStream } from "./stream.js";
+export type {
+	AssistantEvent,
+	ErrorEvent,
+	FileEvent,
+	QueryRequest,
+	ResultEvent,
+	Run,
+	SandcasterClientOptions,
+	SandcasterEvent,
+	SandcasterEventType,
+	StderrEvent,
+	SystemEvent,
+	ThinkingEvent,
+	ToolResultEvent,
+	ToolUseEvent,
+	WarningEvent,
+} from "./types.js";
