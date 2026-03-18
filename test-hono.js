6b03a000bd0b34ba78d209c07e97ee54831aea1b
Ladislav Martincik <ladislav.martincik@gmail.com>
2026-03-18 21:20:29 +0100

    chore: remove Gemini review artifacts

--- test-hono.js
+++
@@ -1,9 +0,0 @@
-const { Hono } = require("hono");
-const { streamSSE } = require("hono/streaming");
-const app = new Hono();
-app.get("/", (c) => streamSSE(c, async (stream) => {
-    c.header("X-Custom", "123");
-    await stream.writeSSE({ event: "test", data: "data" });
-}));
-const req = new Request("http://localhost/");
-app.request(req).then(res => console.log(res.headers.get("X-Custom"))).catch(e => console.error(e));
